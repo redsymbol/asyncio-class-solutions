e9a23eb5d8a24a205f89e54a3305d9bf00554a26
Aaron Maxwell <amax@redsymbol.net>
2014-09-16 23:04:25 -0700

    Corrected task example

--- echo_server_task.py
+++ echo_server_task.py
@@ -1,26 +1,34 @@
+import sys
 import asyncio
+PORT = 4242
 
-PORT=4242
+NUM_CLIENTS = 0
+
+def log(msg):
+    sys.stdout.write(msg + '\n')
+    sys.stdout.flush()
+
+def client_connected_handler(client_reader, client_writer):
+    task = asyncio.Task(handle_client(client_reader, client_writer))
+    def client_done(future):
+        global NUM_CLIENTS
+        NUM_CLIENTS += 1
+        log('Client done')
+    task.add_done_callback(client_done)
 
 @asyncio.coroutine
 def handle_client(client_reader, client_writer):
-    while True:
+    log('New client')
+    while not client_reader.at_eof():
         data = (yield from client_reader.readline())
-        print(data)
-        client_writer.write(b'echo:' + data)
-
-def client_connected_handler(client_reader, client_writer):
-    task = asyncio.async(handle_client(client_reader, client_writer))
-
-    def client_done(task):
-        print('Connection completed')
-
-    task.add_done_callback(client_done)
+        client_writer.write(data)
 
 loop = asyncio.get_event_loop()
 pending_server = asyncio.start_server(client_connected_handler, 'localhost', PORT)
 server = loop.run_until_complete(pending_server)
 try:
     loop.run_forever()
+except KeyboardInterrupt:
+    print('\n{} Clients Served'.format(NUM_CLIENTS))
 finally:
     loop.close()
