97df786ece00bae57ecc223c3f1545b5442a178d
Aaron Maxwell <amax@redsymbol.net>
2014-09-16 23:13:29 -0700

    Refinements to echo task server

--- echo_server_task.py
+++ echo_server_task.py
@@ -1,15 +1,13 @@
-import sys
 import asyncio
 PORT = 4242
-
 NUM_CLIENTS = 0
+def client_done(future):
+    global NUM_CLIENTS
+    NUM_CLIENTS += 1
+    print('Client done')
 
 def client_connected_handler(client_reader, client_writer):
     task = asyncio.Task(handle_client(client_reader, client_writer))
-    def client_done(future):
-        global NUM_CLIENTS
-        NUM_CLIENTS += 1
-        print('Client done')
     task.add_done_callback(client_done)
 
 @asyncio.coroutine
@@ -17,7 +15,8 @@ def handle_client(client_reader, client_writer):
     print('New client')
     while not client_reader.at_eof():
         data = (yield from client_reader.readline())
-        client_writer.write(data)
+        print(data)
+        client_writer.write(b'echo:' + data)
 
 loop = asyncio.get_event_loop()
 pending_server = asyncio.start_server(client_connected_handler, 'localhost', PORT)
